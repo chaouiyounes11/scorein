036f9822fdeebedfb019cb6a40232ee51d2a5dac
Younes Chaoui <chaouiyounes11@gmail.com>
2017-09-01 09:22:59 +0200

    improved UX

--- js/main.js
+++ js/main.js
@@ -5,18 +5,44 @@
 
 function composition() {
   if (document.getElementById('compo').value == "2") {
-    $(".troismilieu").css('display', "none");
-    $('.troisattaquant').css('display', 'block');
-    $(".troisdefenseur").css('display', "block");
+    $("#teamOne .troismilieu").css('display', "none");
+    $('#teamOne .troisattaquant').css('display', 'block');
+    $("#teamOne .troisdefenseur").css('display', "block");
   } else if (document.getElementById('compo').value == "1") {
-    $(".troismilieu").css('display', "block");
-    $('.troisattaquant').css('display', 'none');
-    $(".cinqmilieu").css('display', "none");
-    $(".troisdefenseur").css('display', "block");
+    $('#teamOne .troisattaquant').css('display', 'none');
+    $("#teamOne .troismilieu").css('display', "block");
+    $('#teamOne .troisattaquant').css('display', 'none');
+    $("#teamOne .cinqmilieu").css('display', "none");
+    $("#teamOne .troisdefenseur").css('display', "block");
   } else if (document.getElementById('compo').value == "3") {
-    $(".cinqmilieu").css('display', "block");
-    $(".troismilieu").css('display', "block");
-    $(".troisdefenseur").css('display', "none");
-    $('.troisattaquant').css('display', 'none');
+    $("#teamOne .cinqmilieu").css('display', "block");
+    $("#teamOne .troismilieu").css('display', "block");
+    $("#teamOne .troisdefenseur").css('display', "none");
+    $('#teamOne .troisattaquant').css('display', 'none');
   }
+}
+
+function composition2() {
+  if (document.getElementById('compo1').value == "5") {
+    $("#teamTwo .troismilieu").css('display', "none");
+    $('#teamTwo .troisattaquant').css('display', 'block');
+    $("#teamTwo .troisdefenseur").css('display', "block");
+  } else if (document.getElementById('compo1').value == "4") {
+    $('#teamTwo .troisattaquant').css('display', 'none');
+    $("#teamTwo .troismilieu").css('display', "block");
+    $('#teamTwo .troisattaquant').css('display', 'none');
+    $("#teamTwo .cinqmilieu").css('display', "none");
+    $("#teamTwo .troisdefenseur").css('display', "block");
+  } else if (document.getElementById('compo1').value == "6") {
+    $("#teamTwo .cinqmilieu").css('display', "block");
+    $("#teamTwo .troismilieu").css('display', "block");
+    $("#teamTwo .troisdefenseur").css('display', "none");
+    $('#teamTwo .troisattaquant').css('display', 'none');
+  }
+}
+
+
+
+function info(joueur) {
+  alert(["Nom: Chaoui", " Prenom: Younes", " Position: gardien"]);
 }
